5f2e18b14f98a5af2d2d3e492f4042c47a4df129
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-08-29 14:39:51 +0530

    ....

---
+++ basics/StringReverse.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class StringReverse
+{
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         String name;
+         char[] nameArray;
+
+         System.out.print("Enter a name : ");
+         name = sc.nextLine();
+
+         name = name.trim();
+         nameArray = name.toCharArray();
+
+         for(char i : nameArray)
+         {
+            System.out.print(i);
+         }
+         System.out.println();
+         for (int i=nameArray.length-1; i>=0; i--) {
+             System.out.print(nameArray[i]);
+         }
+
+    }
+}

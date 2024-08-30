f837cd7032932bff959a9ab10b10f2dff96dcb37
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-08-30 12:58:31 +0530

    ....

--- basics/ConsecutiveArray.java
+++ basics/ConsecutiveArray.java
@@ -3,7 +3,7 @@ public class ConsecutiveArray {
 
     public static void main(String[] args) {
         Solution s1 = new Solution();
-        int[] nums = new int[]{1,0,1,1,0,1};
+        int[] nums = new int[]{1,0,1,1,1};
         int ans = s1.findMaxConsecutiveOnes(nums);
         System.out.println(ans);
     }
@@ -12,15 +12,25 @@ public class ConsecutiveArray {
 class Solution {
 
     public int findMaxConsecutiveOnes(int[] nums) {
-        int n = 0;
+        int n = 0,j=0;
+        int[] ans = new int[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    n++;
-                } else {
-                    break;
-                }
+            if(nums[i]==1){
+                n++;
+                ans[j] = n;
             }
+            else{
+                n=0;
+                j++;
+            }
+        }
+        n=0;
+        for (int i = 0; i < j; i++) {
+            System.out.println(ans[i]);
+           if (n<ans[i]) {
+                n=ans[i];
+           }
         }
         return n;
     }
